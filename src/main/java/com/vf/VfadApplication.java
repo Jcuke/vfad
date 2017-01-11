3aa29dfd7ce63fd4e9f82d18906818c9072c166d
wangjinwen <wangjinwen@netfinworks.com>
2017-01-11 10:13:55 +0800

    使用内部tomcat跑起项目, 先得在gradle-->build-->war点击生成war包 再在右击工程open module setting-->Web Gradle-->Web Resource Directories配置webapp目录,否则默认静态资源会从resources中找

--- src/main/java/com/vf/VfadApplication.java
+++ src/main/java/com/vf/VfadApplication.java
@@ -6,32 +6,32 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-/*@SpringBootApplication
+@SpringBootApplication
 public class VfadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VfadApplication.class, args);
 	}
-}*/
+}
 
 
 
 //使用外部tomcat用此写法
-@SpringBootApplication
-// 开启通用注解扫描
-@ComponentScan
-public class VfadApplication extends SpringBootServletInitializer {
-	/**
-	 * 实现SpringBootServletInitializer可以让spring-boot项目在web容器中运行
-	 */
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		builder.sources(this.getClass());
-		return super.configure(builder);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(VfadApplication.class, args);
-
-	}
-}
+//@SpringBootApplication
+//// 开启通用注解扫描
+//@ComponentScan
+//public class VfadApplication extends SpringBootServletInitializer {
+//	/**
+//	 * 实现SpringBootServletInitializer可以让spring-boot项目在web容器中运行
+//	 */
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		builder.sources(this.getClass());
+//		return super.configure(builder);
+//	}
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(VfadApplication.class, args);
+//
+//	}
+//}
